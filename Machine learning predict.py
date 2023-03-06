8823b4548bfeb98e1127310ee0da67e376791b06
Olof Li <ololix050313@student.jenseneducation.se>
2023-03-06 11:27:29 +0100

    its good

---
+++ Machine learning predict.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pn
+
+import plotly.express as px
+import plotly.graph_objects as po
+import plotly.io as pio
+
+import seaborn as sea
+import matplotlib.pyplot as map
+
+As = pn.read_csv("adult.data")
+print(As.to_string())
+
+
