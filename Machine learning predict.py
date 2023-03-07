3dff379731af39552989d190ad2a49eb31801e35
Olof Li <ololix050313@student.jenseneducation.se>
2023-03-07 15:10:13 +0100

    progress

--- Machine learning predict.py
+++ Machine learning predict.py
@@ -8,7 +8,19 @@ import plotly.io as pio
 import seaborn as sea
 import matplotlib.pyplot as map
 
-As = pn.read_csv("adult.data")
-print(As.to_string())
+""" data set Source:
+Donor:
+
+Ronny Kohavi and Barry Becker
+Data Mining and Visualization
+Silicon Graphics.
+e-mail: ronnyk '@' live.com for questions. """
+cols = ["Age", "Workclass", "fnlwgt", "education", "education-num", "marital-status", "occupation", "relationship", "race", "sex", "capital-gain", "capital-loss", "hours-per-week", "native-country", "earnings'"]
+df = pn.read_csv("adult.data", names = cols)
+df = df.head()
+
+
+print(df.to_string())
+
 
 
